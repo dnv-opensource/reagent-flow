1c150fcebbc158f2a0156ecb866441a7a2bbba1e
Kjerringvåg <Henrik.Kjerringvag@dnv.com>
2023-01-27 16:19:53 +0100

    Use use-package instead

--- .github/workflows/init.el
+++ .github/workflows/init.el
@@ -3,23 +3,33 @@
 ;; publishing the documentation.
 
 (prefer-coding-system 'utf-8)
-(dolist (package '("s.el" "dash.el" "clojure-mode" "cider"))
-  (add-to-list 'load-path (expand-file-name package ".github/workflows/packages")))
 
-(require 's)
-(require 'dash)
-(require 'clojure-mode)
-(require 'cider)
-(require 'org)
-(require 'ob-lob)
-(require 'ob-tangle)
-(require 'ob-clojure)
-(require 'org-babel-clojure)
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
 
-(setq org-babel-clojure-backend 'cider
-      org-confirm-babel-evaluate nil)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- (append org-babel-load-languages '((clojure . t)
-                                    (emacs-lisp . t))))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
+(use-package s
+  :ensure t)
+(use-package dash)
+(use-package clojure-mode)
+(use-package cider)
+(use-package org
+  :config
+  (setq org-babel-clojure-backend 'cider
+        org-confirm-babel-evaluate nil)
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   (append org-babel-load-languages '((clojure . t)
+                                      (emacs-lisp . t)))))
