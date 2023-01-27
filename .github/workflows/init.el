a71607e81f3e834b344629ddde830af5602347ea
Henrik Kjerringvåg <hkjels@me.com>
2023-01-27 16:05:49 +0100

    Ci (#17)

--- .github/workflows/init.el
+++ .github/workflows/init.el
@@ -3,16 +3,21 @@
 ;; publishing the documentation.
 
 (prefer-coding-system 'utf-8)
-(dolist (package '("s.el" "dash.el" "clojure-mode"))
+(dolist (package '("s.el" "dash.el" "clojure-mode" "cider"))
   (add-to-list 'load-path (expand-file-name package ".github/workflows/packages")))
 
 (require 's)
 (require 'dash)
 (require 'clojure-mode)
+(require 'cider)
 (require 'org)
 (require 'ob-lob)
 (require 'ob-tangle)
-(setq-default org-confirm-babel-evaluate nil)
+(require 'ob-clojure)
+(require 'org-babel-clojure)
+
+(setq org-babel-clojure-backend 'cider
+      org-confirm-babel-evaluate nil)
 (org-babel-do-load-languages
  'org-babel-load-languages
  (append org-babel-load-languages '((clojure . t)
